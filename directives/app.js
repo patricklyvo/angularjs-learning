76a979b8755907d28bbfd381f11966866455ae33
Patrick Ly-Vo <patricklyvo@gmail.com>
2016-03-16 00:46:48 -0700

    Directive restrictions

--- directives/app.js
+++ directives/app.js
@@ -3,7 +3,59 @@ angular.module('greetings', []);
 angular.module('greetings')
   .directive('welcome', function() {
     return {
+      restrict: 'A',
+      link: function() {
+        alert("Howdy!");
+      }
+    }
+  })
+  .directive('goodbye', function() {
+    return {
+      restrict: 'A',
+      link: function() {
+        alert("See ya later!");
+      }
+    }
+  });
+
+// comment
+/*angular.module('greetings')
+  .directive('welcome', function() {
+    return {
+      restrict: 'M',
+      link: function() {
+        alert("Howdy!");
+      }
+    }
+  });*/
+
+// class
+/*angular.module('greetings')
+  .directive('welcome', function() {
+    return {
+      restrict: 'C',
+      link: function() {
+        alert("Howdy!");
+      }
+    }
+  });*/
+
+// attribute
+/*angular.module('greetings')
+  .directive('welcome', function() {
+    return {
+      restrict: 'A',
+      link: function() {
+        alert("Howdy!");
+      }
+    }
+  });*/
+
+// element
+/*angular.module('greetings')
+  .directive('welcome', function() {
+    return {
       restrict: 'E',
       template: '<div>Howdy there! You look splendid.</div>'
     }
-  });
+  });*/
