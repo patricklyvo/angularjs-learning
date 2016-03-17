9c0d6d033b8a52740b88305f50a1e7211439d1ba
Patrick Ly-Vo <patricklyvo@gmail.com>
2016-03-16 19:44:39 -0700

    Directives talking to controllers

---
+++ directives/app3.js
@@ -0,0 +1,24 @@
+function FunCtrl() {
+  var self = this;
+
+  self.start = function() {
+    console.log('Fun times have been started!');
+  };
+
+  self.end = function() {
+    console.log('Fun time is over.');
+  };
+
+};
+
+angular.module('coolApp', [])
+  .controller('FunCtrl', FunCtrl)
+  .directive('entering', function() {
+    return function(scope, element, attrs) {
+      element.bind('mouseenter', function() {
+        //scope.fun.start();
+        //scope.$apply("fun.start()");
+        scope.$apply(attrs.entering);
+      });
+    }
+  });
