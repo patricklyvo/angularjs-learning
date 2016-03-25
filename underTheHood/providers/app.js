f8e257662aa55c31146651999ffc7f5624ec4473
Patrick Ly-Vo <patricklyvo@gmail.com>
2016-03-24 18:26:11 -0700

    Providers

---
+++ underTheHood/providers/app.js
@@ -0,0 +1,55 @@
+var app = angular.module('app', []);
+
+app.provider('game', function() {
+  var type;
+  return {
+    setType: function(value) {
+      type = value;
+    },
+    $get: function() {
+      return {
+        title: type + 'Craft'
+      };
+    }
+  };
+});
+
+app.config(function(gameProvider) {
+  gameProvider.setType('War');
+});
+
+/*
+app.config(function($provide) {
+  $provide.provider('game', function () {
+    return {
+      $get: function () {
+        return {
+          title: 'StarCraft'
+        };
+      }
+    };
+  });
+});
+*/
+
+/*
+app.config(function($provide) {
+  $provide.factory('game', function() {
+    return {
+      title: 'StarCraft'
+    };
+  });
+});
+*/
+
+/*
+app.factory('game', function() {
+  return {
+    title: 'StarCraft'
+  };
+});
+*/
+
+app.controller('AppCtrl', function($scope, game) {
+  $scope.title = game.title;
+});
